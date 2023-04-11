18bf59d79b105013ae05ac2745ca5ca810385dc4
Daniel Qolami <111076883+DanielQolami@users.noreply.github.com>
2023-04-11 21:20:15 +0330

    env variables don't work without VUE_APP_ in vue.js

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore, collection, enableIndexedDbPersistence } from 'firebase/f
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGE_SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGE_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
 };
 
 // Initialize Firebase
